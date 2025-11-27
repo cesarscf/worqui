12c643939c2520835ee65035afd910b01e4112d4
cesarscf <cesarscfilho@gmail.com>
2025-11-27 20:56:57 -0300

    feat: Add tanstack query and tanstack router setup

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,49 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { createRouter, RouterProvider } from "@tanstack/react-router"
+import { AxiosError } from "axios"
+import React from "react"
+
+import { toast } from "sonner"
+import { unknownError } from "@/lib/constants"
+import { routeTree } from "@/router-tree.gen"
+import { Toaster } from "./components/ui/sonner"
+
+const router = createRouter({ routeTree })
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router
+  }
+}
+
 export function App() {
-  return <>Hello World</>
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 60 * 5,
+          },
+          mutations: {
+            onError(error) {
+              if (error instanceof AxiosError) {
+                const message = error.response?.data?.message ?? unknownError
+
+                toast.error(message)
+                return
+              }
+
+              toast.error(unknownError)
+            },
+          },
+        },
+      }),
+  )
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <Toaster richColors />
+    </QueryClientProvider>
+  )
 }
